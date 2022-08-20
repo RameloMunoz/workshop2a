27a7a4c84c805e5f6be59cd8199c709b4d46749b
Ramelo Munoz <ramelomunoz@gmail.com>
2022-08-20 12:14:50 -0700

    2nd commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
